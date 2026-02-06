dea0ea9b5b9ee42c45a135d3c20c03cb5bf08d2a
sharmila-RS <shrmilar.22bsr@kongu.edu>
2026-02-06 12:39:27 +0530

    Fix dotenv load order for RDS connection

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config(); // MUST be first
+
+import express from "express";
+import cors from "cors";
 import { connectDB } from "./Db/db.js";
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
 import userRoute from "./Routes/userRoutes.js";
 import authUserRoute from "./Routes/authUserRoutes.js";
-dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
-// middlware 
-app.use(express.json())
-app.use(cors())
-//connectivity
-connectDB()
-// routes definition
-app.use('/api/user', userRoute);
-app.use('/api/auth', authUserRoute)
-// http://localhost:5000/api/user/sighup
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// DB connection
+connectDB();
+
+// routes
+app.use("/api/user", userRoute);
+app.use("/api/auth", authUserRoute);
+
 app.listen(PORT, () => {
-    console.log(`your server is runing in ${PORT}`);
-})
+  console.log(`your server is running in ${PORT}`);
+});
